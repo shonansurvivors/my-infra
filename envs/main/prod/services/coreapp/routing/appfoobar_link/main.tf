ed9ba50f3d33bc8f99f1551968d7a955fb021227
shonansurvivors <shonansurvivors@gmail.com>
2022-01-15 22:34:53 +0900

    import

--- envs/main/prod/services/coreapp/routing/appfoobar_link/main.tf
+++ envs/main/prod/services/coreapp/routing/appfoobar_link/main.tf
@@ -1,3 +1,13 @@
 resource "aws_route53_zone" "this" {
   name = "appfoobar.link"
 }
+
+resource "aws_acm_certificate" "root" {
+  domain_name = aws_route53_zone.this.name
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  provider = aws.us-east-1
+}
