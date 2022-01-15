614ff7d0014081508c4d24520d5aeb3e1e7b773e
shonansurvivors <shonansurvivors@gmail.com>
2022-01-15 23:14:28 +0900

    rename

--- envs/main/prod/services/coreapp/routing/appfoobar_link/main.tf
+++ envs/main/prod/services/coreapp/routing/appfoobar_link/main.tf
@@ -4,8 +4,10 @@ resource "aws_route53_zone" "this" {
   force_destroy = false
 }
 
-resource "aws_acm_certificate" "root" {
-  domain_name = aws_route53_zone.this.name
+resource "aws_acm_certificate" "wild_root_us_east_1" {
+  domain_name = "*.${aws_route53_zone.this.name}"
+
+  validation_method = "DNS"
 
   lifecycle {
     create_before_destroy = true
@@ -14,13 +16,9 @@ resource "aws_acm_certificate" "root" {
   provider = aws.us-east-1
 }
 
-resource "aws_acm_certificate_validation" "root" {
-  certificate_arn = aws_acm_certificate.root.arn
-}
-
-resource "aws_route53_record" "root_certificate_validation" {
+resource "aws_route53_record" "wild_root_us_east_1_certificate_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.root.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.wild_root_us_east_1.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       type   = dvo.resource_record_type
       record = dvo.resource_record_value
@@ -33,4 +31,3 @@ resource "aws_route53_record" "root_certificate_validation" {
   type    = each.value.type
   zone_id = aws_route53_zone.this.id
 }
-
