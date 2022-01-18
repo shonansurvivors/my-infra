86db0ed2a16cabba4b453e64b24a51203c3fdbfa
shonansurvivors <shonansurvivors@gmail.com>
2022-01-18 23:25:28 +0900

    manage ou

---
+++ envs/master/aws_organizations/main.tf
@@ -0,0 +1,26 @@
+data "aws_organizations_organization" "this" {}
+
+resource "aws_organizations_organizational_unit" "sandbox" {
+  name      = "Sandbox"
+  parent_id = data.aws_organizations_organization.this.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "security" {
+  name      = "Security"
+  parent_id = data.aws_organizations_organization.this.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "workloads" {
+  name      = "Workloads"
+  parent_id = data.aws_organizations_organization.this.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "workloads_prod" {
+  name      = "Workloads-Prod"
+  parent_id = aws_organizations_organizational_unit.workloads.id
+}
+
+resource "aws_organizations_organizational_unit" "workloads_test" {
+  name      = "Workloads-Test"
+  parent_id = aws_organizations_organizational_unit.workloads.id
+}
