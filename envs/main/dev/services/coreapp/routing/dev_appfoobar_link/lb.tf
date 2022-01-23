5245502ee88d2aa4637ad27f8a55d8614fbf4e04
shonansurvivors <shonansurvivors@gmail.com>
2022-01-24 00:20:11 +0900

    create lb

--- envs/main/dev/services/coreapp/routing/dev_appfoobar_link/lb.tf
+++ envs/main/dev/services/coreapp/routing/dev_appfoobar_link/lb.tf
@@ -1,18 +1,14 @@
 resource "aws_lb" "this" {
-
   name = "main-dev-appfoobar-link"
 
   internal           = false
   load_balancer_type = "application"
 
   security_groups = [
-    data.terraform_remote_state.network_main.outputs.security_group_web_id,
-    data.terraform_remote_state.network_main.outputs.security_group_vpc_id
+    data.aws_security_group.web.id
   ]
 
-  subnets = [
-    for s in data.terraform_remote_state.network_main.outputs.subnet_public : s.id
-  ]
+  subnets = data.aws_subnet_ids.public.ids
 
   tags = {
     Name = "main-dev-appfoobar-link"
