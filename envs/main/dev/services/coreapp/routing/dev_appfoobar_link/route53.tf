d7f79deb056c6a20e9db1946c76e5ad086f6da9e
shonansurvivors <shonansurvivors@gmail.com>
2022-01-24 00:34:20 +0900

    create records

--- envs/main/dev/services/coreapp/routing/dev_appfoobar_link/route53.tf
+++ envs/main/dev/services/coreapp/routing/dev_appfoobar_link/route53.tf
@@ -3,3 +3,27 @@ resource "aws_route53_zone" "this" {
 
   force_destroy = false
 }
+
+resource "aws_route53_record" "foo_a" {
+  name    = "foo.${aws_route53_zone.this.name}"
+  type    = "A"
+  zone_id = aws_route53_zone.this.id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_lb.this.dns_name
+    zone_id                = aws_lb.this.zone_id
+  }
+}
+
+resource "aws_route53_record" "bar_a" {
+  name    = "bar.${aws_route53_zone.this.name}"
+  type    = "A"
+  zone_id = aws_route53_zone.this.id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_lb.this.dns_name
+    zone_id                = aws_lb.this.zone_id
+  }
+}
