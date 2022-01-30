f5c55105e2e315bf1852f19bc7aee4f4ef8492f6
shonansurvivors <shonansurvivors@gmail.com>
2022-01-30 15:31:14 +0900

    Since public

--- envs/main/dev/foundation/security_groups/main.tf
+++ envs/main/dev/foundation/security_groups/main.tf
@@ -5,6 +5,7 @@ data "aws_vpc" "main" {
   }
 }
 
+
 resource "aws_security_group" "web" {
   name   = "main-web"
   vpc_id = data.aws_vpc.main.*.id[0]
@@ -13,21 +14,21 @@ resource "aws_security_group" "web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-egress-sgr
   }
 
   tags = {
