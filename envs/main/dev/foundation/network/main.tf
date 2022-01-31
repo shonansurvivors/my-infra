fbcf518fdb151d1f8a45a8ddf3845e3a60e6f6ea
shonansurvivors <shonansurvivors@gmail.com>
2022-01-31 20:24:29 +0900

    S3 Access Block should Ignore Public Acl

--- envs/main/dev/foundation/network/main.tf
+++ envs/main/dev/foundation/network/main.tf
@@ -60,4 +60,5 @@ resource "aws_s3_bucket_public_access_block" "vpc_flow_logs" {
   bucket              = aws_s3_bucket.vpc_flow_logs.id
   block_public_acls   = true
   block_public_policy = true
+  ignore_public_acls  = true
 }
