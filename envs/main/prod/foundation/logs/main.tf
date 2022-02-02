6fb4cb4310ad8a064c3b37f87f4fdc1e8fb29c06
shonansurvivors <shonansurvivors@gmail.com>
2022-02-02 15:11:34 +0900

    S3 Access block should block public

--- envs/main/prod/foundation/logs/main.tf
+++ envs/main/prod/foundation/logs/main.tf
@@ -33,3 +33,11 @@ resource "aws_s3_bucket" "cloudfront_logs" {
     }
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "cloudfront_logs" {
+  bucket                  = aws_s3_bucket.cloudfront_logs.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
